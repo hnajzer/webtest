e9ea4c1b81c16cc824947b1288d589953b368006
Helga Zsikai-Najzer <helga.zsikainajzer@logmein.com>
2019-03-14 16:47:29 +0100

    Extend HomePage PO

--- src/test/java/com/logmein/testing/pageobjects/HomePage.java
+++ src/test/java/com/logmein/testing/pageobjects/HomePage.java
@@ -1,10 +1,13 @@
 package com.logmein.testing.pageobjects;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class HomePage {
 	
     private final WebDriver browser;
+    By signInButtonLocator = By.cssSelector("a.login");
+    By myAccountButtonLocator = By.cssSelector("a.account");
 
     public HomePage(WebDriver driver) {
         this.browser = driver;
@@ -15,5 +18,8 @@ public class HomePage {
         }
     }
 
-
+    public LoginPage signIn(String email, String password) {
+    	browser.findElement(signInButtonLocator).click();
+        return new LoginPage(browser);
+    }
 }
